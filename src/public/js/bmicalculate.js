3d8f98e3950d52197dcb97cc565476538144754e
MarcuXNg <trungkien3803@gmail.com>
2023-10-26 13:36:53 +0700

    age noinput warning

--- src/public/js/bmicalculate.js
+++ src/public/js/bmicalculate.js
@@ -3,15 +3,18 @@ function bmicalculate() {
   const weight = parseFloat(document.getElementById('weight').value);
   const height = parseFloat(document.getElementById('height').value);
 
-  const weightNoti = document.getElementById('weightNoti');
+  const age = parseFloat(document.getElementById('age').value);
 
+  const weightNoti = document.getElementById('weightNoti');
   const heightNoti = document.getElementById('heightNoti');
+  const ageNoti = document.getElementById('ageNoti');
 
   // Reset notifications
   weightNoti.style.display = 'none';
   heightNoti.style.display = 'none';
+  ageNoti.style.display = 'none';
 
-  // condition
+  // Condition
   if (isNaN(weight)) {
   // Show the notification
     weightNoti.style.display = 'block';
@@ -22,13 +25,18 @@ function bmicalculate() {
     heightNoti.style.display = 'block';
   }
 
+  if (isNaN(age)) {
+  // Show the notification
+    ageNoti.style.display = 'block';
+  }
+
   if (isNaN(weight) && isNaN(height)) {
     // Show the notification
     weightNoti.style.display = 'block';
     heightNoti.style.display = 'block';
   }
 
-  if (!isNaN(weight) && !isNaN(height)) {
+  if (!isNaN(weight) && !isNaN(height) && !isNaN(age)) {
     const heightInMeters = height / 100; // Convert height from centimeters to meters
     const bmi = weight / (heightInMeters * heightInMeters);
     // bmi rounded number
